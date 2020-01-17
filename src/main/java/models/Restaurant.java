21317f5f58a6528f35a7dd74e44ab0f0421d06ff
Thomasmaghanga <wavyylikethesea@gmail.com>
2020-01-17 09:53:14 +0300

    Created a POJO including constructor overloading for the Restaurant model class

---
+++ src/main/java/models/Restaurant.java
@@ -0,0 +1,27 @@
+package models;
+
+public class Restaurant {
+    private String name;
+    private String address;
+    private String zipcode;
+    private String phone;
+    private String website;
+    private String email;
+
+
+    public Restaurant(String name, String address, String zipcode, String phone) {
+        this.name = name;
+        this.address = address;
+        this.zipcode = zipcode;
+        this.phone = phone;
+    }
+
+    public Restaurant(String name, String address, String zipcode, String phone, String website, String email) {
+        this.name = name;
+        this.address = address;
+        this.zipcode = zipcode;
+        this.phone = phone;
+        this.website = website;
+        this.email = email;
+    }
+}
