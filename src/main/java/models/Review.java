88c364d2270aa63588eb84616adf31a66426a84c
Thomasmaghanga <wavyylikethesea@gmail.com>
2020-01-17 10:06:10 +0300

    Create POJO and overload constructors for the Review class

--- src/main/java/models/Review.java
+++ src/main/java/models/Review.java
@@ -1,5 +1,16 @@
 package models;
 
 public class Review {
+    private String content;
+    private String writtenBy;
+    private int rating;
+    private int id;
+    private int restaurantId; //to connect Restaurant to Review (one-to-many)
 
+    public Review(String content, String writtenBy, int rating, int restaurantId) {
+        this.content = content;
+        this.writtenBy = writtenBy;
+        this.rating = rating;
+        this.restaurantId = restaurantId;
+    }
 }
