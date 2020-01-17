66e902db8fa76960150dd563debc6709661896a1
Thomasmaghanga <wavyylikethesea@gmail.com>
2020-01-17 10:28:49 +0300

    Add getters and setters for the Foodtype model class

--- src/main/java/models/Foodtype.java
+++ src/main/java/models/Foodtype.java
@@ -7,4 +7,20 @@ public class Foodtype {
     public Foodtype(String name) {
         this.name = name;
     }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
 }
