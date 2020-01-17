0e2ac7385a12befb1d356b9e0d1edcc20162a43d
Thomasmaghanga <wavyylikethesea@gmail.com>
2020-01-17 10:09:43 +0300

    Add getters and setters to the Review model class

--- src/main/java/models/Review.java
+++ src/main/java/models/Review.java
@@ -13,4 +13,44 @@ public class Review {
         this.rating = rating;
         this.restaurantId = restaurantId;
     }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public String getWrittenBy() {
+        return writtenBy;
+    }
+
+    public void setWrittenBy(String writtenBy) {
+        this.writtenBy = writtenBy;
+    }
+
+    public int getRating() {
+        return rating;
+    }
+
+    public void setRating(int rating) {
+        this.rating = rating;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getRestaurantId() {
+        return restaurantId;
+    }
+
+    public void setRestaurantId(int restaurantId) {
+        this.restaurantId = restaurantId;
+    }
 }
